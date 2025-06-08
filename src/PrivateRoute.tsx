a9bce3819f68747aaee5940cf2c7e44c1cc91b15
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2025-06-09 00:23:03 +0100

    autho to login

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const userId = localStorage.getItem("user_id");
-  return userId ? children : <Navigate to="/auth" replace />;
+  return userId ? children : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
